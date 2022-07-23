7ba1b050b2090978bb02ddc7a34309a8f4a4540c
Cristian Quiroz <cristian@176lab.com>
2022-07-23 16:53:45 -0400

    add default modules

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,132 @@ const webpack = require('webpack');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
-const dirAssets = path.join(__dirname, 'assets')
+const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
+const dirNode = 'node_modules'
+
+module.exports = {
+    entry: [
+        path.join(dirApp, 'index.js'),
+        path.join(dirStyles, 'index.scss'),
+    ],
+    resolve: {
+        modules: [
+            dirApp,
+            dirShared,
+            dirStyles,
+            dirNode
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEVELOPMENT
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './shared',
+                    to: ''
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+                loader: 'file-loader',
+                options: {
+                    name(file) {
+                        return '[hash].[ext]'
+                    }
+                }
+            }, 
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                type: "asset",
+            },            
+        ]
+    },  
+    optimization: {
+        minimizer: [
+            "...",
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        // Lossless optimization with custom option
+                        // Feel free to experiment with options for better result for you
+                        plugins: [
+                            ["gifsicle", {
+                                interlaced: true
+                            }],
+                            ["jpegtran", {
+                                progressive: true
+                            }],
+                            ["optipng", {
+                                optimizationLevel: 5
+                            }],
+                            // Svgo configuration here https://github.com/svg/svgo#configuration
+                            [
+                                "svgo",
+                                {
+                                    plugins: [
+                                        {
+                                            name: "removeViewBox",
+                                            active: false,        
+                                        },
+                                        {
+                                            name: "addAttributesToSVGElement",
+                                            params: {
+                                                attributes: [{
+                                                    xmlns: "http://www.w3.org/2000/svg"
+                                                }],
+                                            },        
+                                        }    
+                                    ]                                    
+                                },
+                            ],
+                        ],
+                    },
+                },
+            }),
+        ],
+    },    
+}
